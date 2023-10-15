6c1791e7f61a0182b9eb01d7dfea5030d22a3844
Shivam Kumar <68140446+shivamsks219@users.noreply.github.com>
2023-10-15 09:42:55 +0530

    create fetch_data function

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 #flask app for clipboard
+import win32clipboard
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def fetch_data():
+    win32clipboard.OpenClipboard()
+	data = win32clipboard.GetClipboardData()
+	win32clipboard.CloseClipboard()
+	return data
